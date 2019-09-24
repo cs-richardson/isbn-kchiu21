6602734bb6750d7b644f29daf1346b44a41f19f7
Kolton Chiu <kchiu21@ssis.edu.vn>
2019-09-24 09:49:40 +0700

    Create isbn.py

---
+++ isbn.py
@@ -0,0 +1,19 @@
+"""
+Code written by Kolton
+In this program, the user puts in a isbn string, then the programs
+checks if the isbn string is either valid or invalid then prints "YES" or "NO". 
+"""
+isbn_input = input("ISBN: ")
+while isbn_input.isdigit() == False:
+    isbn_input = input("Retry: ")
+isbn_input = int(isbn_input)
+sum = 0
+tenth_digit = isbn_input % 10
+for i in range (1, 10):
+    digit_num = isbn_input % (10 ** (i + 1))
+    digit_num = digit_num // (10 ** i)
+    sum = sum + digit_num * (10 - i)
+if sum % 11 == tenth_digit:
+    print ("YES")
+else:
+    print ("NO")
